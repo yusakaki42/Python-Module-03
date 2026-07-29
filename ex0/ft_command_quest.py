e7309b7e1f6091fe588ddb3c4a58edc951bb1ef6
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-07-29 15:41:46 +0900

    make file

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main() -> None:
+    args = sys.argv
+    print("=== Command Quest ===")
+    print(f"Program name: {args[0]}")
+
+    if len(args) == 1:
+        print("No arguments provided!")
+    else:
+        print("Program name: {args[0]}")
+        for i, arg in enumerate(args[1:], start=1):
+            print("Argument {i}: {arg}")
+
+
+    print("Total arguments: {len(args)}")
