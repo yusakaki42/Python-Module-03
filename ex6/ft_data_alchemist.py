5c97506fb9cd2f5a631edf66c8d1ec66e36613a7
Yu Sakakibara <yusakaki@c2r1s1.42tokyo.jp>
2026-08-02 23:12:31 +0900

    complete

---
+++ ex6/ft_data_alchemist.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+import random
+
+
+def main() -> None:
+    print("=== Game Data Alchemist ===")
+
+    players = ['Alice', 'bob', 'Charlie', 'dylan',
+               'Emma', 'Gregory', 'john', 'kevin', 'Liam']
+    print(f"Initial list of players: {players}")
+
+    all_capitalized = [name.capitalize() for name in players]
+    print(f"New list with all names capitalized: {all_capitalized}")
+
+    only_capitalized = [name for name in players if name[0].isupper()]
+    print(f"New list of capitalized names only: {only_capitalized}")
+
+    score_dict = {name: random.randint(0, 999) for name in all_capitalized}
+    print(f"Score dict: {score_dict}")
+
+    average = round(sum(score_dict.values()) / len(score_dict), 2)
+    print(f"Score average is {average}")
+
+    high_scores = {name: score for name, score in score_dict.items()
+                   if score > average}
+    print(f"High scores: {high_scores}")
+
+if __name__ == "__main__":
+    main()
